e941d6563947f2bd61e5c17eea733b4e14e05e8f
chrisalopez1337 <chrisalopez@outlook.com>
2020-09-24 15:50:00 -0700

    Added function to grap room data on mount

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {allRoomData: []};
+
+        this.getRoomData = this.getRoomData.bind(this);
+    }
+
+    getRoomData() {
+        var self = this;
+        axios.get('http://localhost:3000/stays/2')
+            .then( res => {
+                self.setState({allRoomData: res.data}); 
+            })
+            .catch( err => {
+                console.log(`Err @ [ getRoomData ] ::: ${err}`);
+            })
+    }
+
+    componentDidMount() {
+        this.getRoomData();
     }
 
     render() {
+        console.log(this.state.allRoomData);
         return (
             <div>
                 <h1>Hello World</h1>
