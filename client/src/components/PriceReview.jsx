78e30f94d1a21f7892c97cedd8688b0367e1364a
chrisalopez1337 <chrisalopez@outlook.com>
2020-09-25 19:54:04 -0700

    Added some general skeleton to the react architecture, can render data down the chain as needed now using conditional rendering

--- client/src/components/PriceReview.jsx
+++ client/src/components/PriceReview.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
 
-const PriceReview = (props) => {
-    console.log(props.room);
-
-    // let data;
-    // if (props.room !== undefined) {
-    //     data = <h1>{props.room.taxes}</h1>
-    // } else {
-    //     data = <h1>'Loading'</h1>
-    // }
-    let data = props.room !== undefined ? <h1>{props.room.taxes}</h1> : <h1>'Loading'</h1>
-    return (
-    <div>
-        { props.room !== undefined ? <div><h1>{props.room.nightly_price}</h1>/Night<h1></h1></div>: <h1>'Loading...'</h1>}
-        { props.room !== undefined ? <div><h1>{props.room.ratings_sum}</h1><h1>{props.room.ratings_count}</h1></div>: <h1>'Loading...'</h1>}
-    </div>
-    )  
-}
-//ratings_count
-//ratings_sum
-//nightly_price
+const PriceReview = ({ room }) => (
+  <div>
+    {room ? <h1>{room.base_nightly_price}</h1> : <h1>'Loading...'</h1> }
+    {room ? <h1>{room.ratings_sum}</h1> : <h1>'Loading...'</h1> }
+  </div>
+);
 
 export default PriceReview;
