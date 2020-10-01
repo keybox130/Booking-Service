5f6fdfd7af038c829b213b79b1dadcec112ec13a
chrisalopez1337 <chrisalopez@outlook.com>
2020-09-30 17:56:57 -0700

    Saving point, days are rendered in unique components, have hover effects and grid is formatted correctly

--- client/src/components/DateSpan.jsx
+++ client/src/components/DateSpan.jsx
@@ -37,123 +37,148 @@ class DateSpan extends React.Component {
       months: null,
       baseDate: ''
     };
+
+    this.hardCodedMonths = this.hardCodedMonths.bind(this);
   }
+
+
+  hardCodedMonths() {
+    var august = [
+      {week: 'dow', data: [
+        {date: 'Su', type: 'dow', key: 1},
+        {date: 'Mo', type: 'dow', key: 2},
+        {date: 'Tu', type: 'dow', key: 3},
+        {date: 'We', type: 'dow', key: 4},
+        {date: 'Th', type: 'dow', key: 5},
+        {date: 'Fr', type: 'dow', key: 6},
+        {date: 'Sa', type: 'dow', key: 7}
+      ]},
+      {week: 1, data: [
+        {date: 1, type: 'normal', key: 1},
+        {date: 2, type: 'normal', key: 2},
+        {date: 3, type: 'normal', key: 3},
+        {date: 4, type: 'normal', key: 4},
+        {date: 5, type: 'normal', key: 5},
+        {date: 6, type: 'normal', key: 6},
+        {date: 7, type: 'normal', key: 7}
+      ]},
+      {week: 2, data: [
+        {date: 8, type: 'normal', key: 1},
+        {date: 9, type: 'normal', key: 2},
+        {date: 10, type: 'normal', key: 3},
+        {date: 11, type: 'normal', key: 4},
+        {date: 12, type: 'normal', key: 5},
+        {date: 13, type: 'normal', key: 6},
+        {date: 14, type: 'normal', key: 7}
+      ]},
+      {week: 3, data: [
+        {date: 15, type: 'normal', key: 1},
+        {date: 16, type: 'normal', key: 2},
+        {date: 17, type: 'normal', key: 3},
+        {date: 18, type: 'normal', key: 4},
+        {date: 19, type: 'normal', key: 5},
+        {date: 20, type: 'normal', key: 6},
+        {date: 21, type: 'normal', key: 7}
+      ]},
+      {week: 4, data: [
+        {date: 22, type: 'normal', key: 1},
+        {date: 23, type: 'normal', key: 2},
+        {date: 24, type: 'normal', key: 3},
+        {date: 25, type: 'normal', key: 4},
+        {date: 26, type: 'normal', key: 5},
+        {date: 27, type: 'normal', key: 6},
+        {date: 28, type: 'normal', key: 7}
+      ]},
+      {week: 5, data: [
+        {date: 29, type: 'normal', key: 1},
+        {date: 30, type: 'normal', key: 2},
+        {date: '', type: 'empty', key: 3},
+        {date: '', type: 'empty', key: 4},
+        {date: '', type: 'empty', key: 5},
+        {date: '', type: 'empty', key: 6},
+        {date: '', type: 'empty', key: 7}
+      ]}
+    ];
+
+    const october = [
+      {week: 'dow', data: [
+        {date: 'Su', type: 'dow', key: 1},
+        {date: 'Mo', type: 'dow', key: 2},
+        {date: 'Tu', type: 'dow', key: 3},
+        {date: 'We', type: 'dow', key: 4},
+        {date: 'Th', type: 'dow', key: 5},
+        {date: 'Fr', type: 'dow', key: 6},
+        {date: 'Sa', type: 'dow', key: 7}
+      ]},
+      {week: 1, data: [
+        {date: '', type: 'empty', key: 1},
+        {date: '', type: 'empty', key: 2},
+        {date: 1, type: 'normal', key: 3},
+        {date: 2, type: 'normal', key: 4},
+        {date: 3, type: 'normal', key: 5},
+        {date: 4, type: 'normal', key: 6},
+        {date: 5, type: 'normal', key: 7}
+      ]},
+      {week: 2, data: [
+        {date: 6, type: 'normal', key: 1},
+        {date: 7, type: 'normal', key: 2},
+        {date: 8, type: 'normal', key: 3},
+        {date: 9, type: 'normal', key: 4},
+        {date: 10, type: 'normal', key: 5},
+        {date: 11, type: 'normal', key: 6},
+        {date: 12, type: 'normal', key: 7}
+      ]},
+      {week: 3, data: [
+        {date: 13, type: 'normal', key: 1},
+        {date: 14, type: 'normal', key: 2},
+        {date: 15, type: 'normal', key: 3},
+        {date: 16, type: 'normal', key: 4},
+        {date: 17, type: 'normal', key: 5},
+        {date: 18, type: 'normal', key: 6},
+        {date: 19, type: 'normal', key: 7}
+      ]},
+      {week: 4, data: [
+        {date: 20, type: 'normal', key: 1},
+        {date: 21, type: 'normal', key: 2},
+        {date: 22, type: 'normal', key: 3},
+        {date: 23, type: 'normal', key: 4},
+        {date: 24, type: 'normal', key: 5},
+        {date: 25, type: 'normal', key: 6},
+        {date: 26, type: 'normal', key: 7}
+      ]},
+      {week: 5, data: [
+        {date: 27, type: 'normal', key: 1},
+        {date: 28, type: 'normal', key: 2},
+        {date: 29, type: 'normal', key: 3},
+        {date: 30, type: 'normal', key: 4},
+        {date: 31, type: 'normal', key: 5},
+        {date: '', type: 'empty', key: 6},
+        {date: '', type: 'empty', key: 7}
+      ]},
+    ];
+    this.setState({august: august, october: october});
+  }
+
+  componentDidMount() {
+    this.hardCodedMonths();
+  }
+
 
   render() {
     let days = this.state.months ? this.state.months[0].map(day => <Day date={day} />) : <h1>No Dates To Load</h1>;
+    let august = this.state.august ? (
+      this.state.august.map(week => <FlexRow>{week.data.map(day => <Day date={day.date} type={day.type} keys={{week: week.week, key: day.key}}/>)}</FlexRow>)
+    ) : <h1>Loading</h1>;
+    let october = this.state.october ? (
+      this.state.october.map(week => <FlexRow>{week.data.map(day => <Day date={day.date} type={day.type} keys={{week: week.week, key: day.key}} />)}</FlexRow>)
+    ) : <h1>Loading</h1>;
     return(
       <Container>
         <MonthHolder>
-        <FlexRow>
-            <Day date={'Su'} type={'dow'}/>
-            <Day date={'Mo'} type={'dow'}/>
-            <Day date={'Tu'} type={'dow'}/>
-            <Day date={'We'} type={'dow'}/>
-            <Day date={'Th'} type={'dow'}/>
-            <Day date={'Fr'} type={'dow'}/>
-            <Day date={'Sa'} type={'dow'}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={1}/>
-            <Day date={2}/>
-            <Day date={3}/>
-            <Day date={4}/>
-            <Day date={5}/>
-            <Day date={6}/>
-            <Day date={7}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={8}/>
-            <Day date={9}/>
-            <Day date={10}/>
-            <Day date={11}/>
-            <Day date={12}/>
-            <Day date={13}/>
-            <Day date={14}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={15}/>
-            <Day date={16}/>
-            <Day date={17}/>
-            <Day date={18}/>
-            <Day date={19}/>
-            <Day date={20}/>
-            <Day date={21}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={22}/>
-            <Day date={23}/>
-            <Day date={24}/>
-            <Day date={25}/>
-            <Day date={26}/>
-            <Day date={27}/>
-            <Day date={28}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={29}/>
-            <Day date={30}/>
-            <Day date={''}/>
-            <Day date={''}/>
-            <Day date={''}/>
-            <Day date={''}/>
-            <Day date={''}/>
-          </FlexRow>
+          {august}
         </MonthHolder>
         <MonthHolder>
-        <FlexRow>
-            <Day date={'Su'} type={'dow'}/>
-            <Day date={'Mo'} type={'dow'}/>
-            <Day date={'Tu'} type={'dow'}/>
-            <Day date={'We'} type={'dow'}/>
-            <Day date={'Th'} type={'dow'}/>
-            <Day date={'Fr'} type={'dow'}/>
-            <Day date={'Sa'} type={'dow'}/>
-          </FlexRow>
-        <FlexRow>
-            <Day date={''}/>
-            <Day date={''}/>
-            <Day date={1}/>
-            <Day date={2}/>
-            <Day date={3}/>
-            <Day date={4}/>
-            <Day date={5}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={6}/>
-            <Day date={7}/>
-            <Day date={8}/>
-            <Day date={9}/>
-            <Day date={10}/>
-            <Day date={11}/>
-            <Day date={12}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={13}/>
-            <Day date={14}/>
-            <Day date={15}/>
-            <Day date={16}/>
-            <Day date={17}/>
-            <Day date={18}/>
-            <Day date={19}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={20}/>
-            <Day date={21}/>
-            <Day date={22}/>
-            <Day date={23}/>
-            <Day date={24}/>
-            <Day date={25}/>
-            <Day date={26}/>
-          </FlexRow>
-          <FlexRow>
-            <Day date={27}/>
-            <Day date={28}/>
-            <Day date={29}/>
-            <Day date={30}/>
-            <Day date={31}/>
-            <Day date={''}/>
-            <Day date={''}/>
-          </FlexRow>
+          {october}
         </MonthHolder>
       </Container>
     );
--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -43,6 +43,7 @@ const SelectedContainer = styled.div`
   font-size: 14px;
   background-color: #222222;
   color: white;
+  cursor: pointer;
 `;
 
 const ChainedContainer = styled.div`
@@ -71,10 +72,23 @@ const DayOfWeekContainer = styled.div`
   color: #727272;
 `;
 
+const Empty = styled.div`
+  padding: 10px;
+  height: 15px;
+  width: 15px;
+  border-radius: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 14px;
+  color: #727272; 
+`;
+
 class Day extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {day: null, state: null, type: ''}
+    this.state = {day: null, type: ''}
   }
 
   sendClickDate() {
@@ -85,8 +99,12 @@ class Day extends React.Component {
   }
 
   render() {
-    let button = this.state.type === 'dow' ? (<DayOfWeekContainer><p>{this.state.day}</p></DayOfWeekContainer>) 
-      :  (<NormalContainer><p>{this.state.day}</p></NormalContainer>);
+    let button = this.state.type === 'normal' ? (<NormalContainer onClick={this.handleClick}><p>{this.state.day}</p></NormalContainer>)
+      : this.state.type === 'dow' ? (<DayOfWeekContainer ><p>{this.state.day}</p></DayOfWeekContainer>)
+      : this.state.type === 'selected' ? (<SelectedContainer onClick={this.handleClick}><p>{this.state.day}</p></SelectedContainer>)
+      : this.state.type === 'chained' ? (<ChainedContainer onClick={this.handleClick}><p>{this.state.day}</p></ChainedContainer>)
+      : this.state.type === 'used' ? (<TakenContianer onClick={this.handleClick}><p>{this.state.day}</p></TakenContianer>)
+      : this.state.type === 'empty' ? <Empty><p></p></Empty> : <h1>ERROR</h1>
     return (
     <div>
       {button}
