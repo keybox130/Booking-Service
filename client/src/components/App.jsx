e1e3ed2171a16f05e07307c4ac19b3ba487abfa1
chrisalopez1337 <chrisalopez@outlook.com>
2020-10-09 19:42:20 -0700

    Fixed style issues

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,10 +3,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import { createGlobalStyle } from 'styled-components';
 // Components
 import InitialState from './InitialState.jsx';
 import Calendar from './Calendar.jsx';
 import GuestModal from './GuestModal.jsx';
+
+const GlobalStyle = createGlobalStyle`
+html, body {
+  font-family: 'Montserrat', sans-serif;
+  padding: 0;
+  margin: 0;
+}
+
+div, a, h1, h2, h3, h4, h5, h6, p, input, button, option, select, form {
+  font-family: inherit;
+  color: inherit;
+  padding: 0;
+  margin: 0;
+}
+img {
+  cursor: pointer;
+}
+`;
 
 const Container = styled.div`
   position: absolute;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    div, a, h1, h2, h3, h4, h5, h6, p, input, button, option, select, form {
+    font-family: inherit;
+    color: inherit;
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
+<React.Fragment>
+<GlobalStyle />
+  <App />
+</React.Fragment>,
+document.getElementById('app'),
 );
